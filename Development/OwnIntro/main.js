12cdbdeb1299278c6b1589752f94c89bcb9013a2
AeroG <weihung0801@gmail.com>
2021-01-03 01:59:59 +0800

    update ver1.534

--- Development/OwnIntro/main.js
+++ Development/OwnIntro/main.js
@@ -1,4 +1,8 @@
 document.getElementById("up").addEventListener('click',function(){
     window.scrollTo(0,1);
-    console.log("clicked");
+});
+
+$(document).ready(function () {
+    $("body").niceScroll();
+    $(".hr").niceScroll({});
 });
