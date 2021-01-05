724f8e434f57e17558694014ff7268192f20276a
Pan Wei Hung <weihung0801@gmail.com>
2021-01-06 01:13:49 +0800

    update ver1.7

--- Development/Project 6 - Meteor.js/quicktodo/client/main.js
+++ Development/Project 6 - Meteor.js/quicktodo/client/main.js
@@ -1,18 +1,48 @@
 import { Template } from 'meteor/templating';
+import { Accounts } from 'meteor/accounts-base';
 
 import './main.html';
+import '../lib/collections.js';
 
-const todos = [
-  {text:'Pickup package'},
-  {text:'Food Shopping'},
-  {text:'Meet with Friends'}
-]
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY'
+});
+
+Template.main.onCreated(function mainOnCreated(){
+  Meteor.subscribe('todos');
+});
 
 Template.main.helpers({
   title() {
     return 'Quick To Do'
   },
-  todos(){
+  todos() {
+    const todos = Todos.find();
     return todos;
+  },
+  isOwner() {
+    return this.owner === Meteor.userId();
+  }
+});
+
+Template.main.events({
+  'submit .add'(event) {
+    event.preventDefault();
+    const text = event.target.text.value;
+    const time = event.target.time.value;
+
+    Meteor.call('todos.insert',text,time);
+
+    event.target.text.value = '';
+    event.target.time.value = '';
+  },
+  'click .toggle-checked'(event) {
+    Meteor.call('todos.checked',this._id,this.checked);
+  },
+  'click .delete'(event) {
+    Meteor.call('todos.remove',this._id);
+  },
+  'click .toggle-private'(){
+    Meteor.call('todos.setPrivate',this._id,!this.private);
   }
 });
--- Development/Project 6 - Meteor.js/quicktodo/server/main.js
+++ Development/Project 6 - Meteor.js/quicktodo/server/main.js
@@ -1,5 +1,51 @@
 import { Meteor } from 'meteor/meteor';
+import '../lib/collections.js';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+
+Meteor.publish('todos', function todoPublication() {
+  return Todos.find({
+    $or: [
+      { private: { $ne: true } },
+      { owner: this.userId }
+    ]
+  });
+});
+
+Meteor.methods({
+  'todos.insert'(text, time) {
+    if (!this.userId) {
+      throw new Meteor.Error('Not Authorised');
+    }
+
+    Todos.insert({
+      text,
+      time,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+
+  },
+  'todos.checked'(id, checked) {
+    Todos.update(id, {
+      $set: { checked: !checked }
+    });
+  },
+  'todos.remove'(id) {
+    const todo = Todos.findOne(id);
+    
+    if (todo.owner !== this.userId) {
+      throw new Meteor.Error('Unautorised');
+    }
+
+    Todos.remove(id);
+  },
+  'todos.setPrivate'(id, setToPrivate) {
+    const todo = Todos.findOne(id);
+    
+    if (todo.owner !== this.userId) {
+      throw new Meteor.Error('Unautorised');
+    }
+
+    Todos.update(id, { $set: { private: setToPrivate } });
+  }
 });
