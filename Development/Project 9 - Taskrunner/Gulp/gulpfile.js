b40d2ce6cb1abff3b80c3d3a5ebc606e13516d96
Pan Wei Hung <weihung0801@gmail.com>
2021-01-13 23:02:37 +0800

    update ver1.902

--- Development/Project 9 - Taskrunner/Gulp/gulpfile.js
+++ Development/Project 9 - Taskrunner/Gulp/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var webserver = require('gulp-webserver');
 
 //test
 gulp.task('test', function(){
@@ -9,7 +10,7 @@ gulp.task('test', function(){
 
 //minify js file
 gulp.task('minifyjs',function(){
-    return gulp.src('src/*.js').pipe(uglify()).pipe(gulp.dest('dist'));
+    return gulp.src('src/*.js').pipe(uglify()).pipe(gulp.dest('app/js'));
 });
 
 //compile scss to css (unable to install gulp-sass)
@@ -19,5 +20,13 @@ gulp.task('jshint',function(){
     return gulp.src('src/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
 })
 
+gulp.task('webserver',function(){
+    return gulp.src('app').pipe(webserver({
+        port:'8000',
+        livereload:true,
+        open:true
+    }));
+})
+
 //default task
-gulp.task('default',gulp.series('minifyjs','jshint'));
+gulp.task('default',gulp.series('minifyjs','jshint','webserver'));
